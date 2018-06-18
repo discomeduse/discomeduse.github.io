5a8821e105dbe1ecea69d6912b6abbef7f3ce192
nibruuu <36766244+nibruuu@users.noreply.github.com>
2018-06-18 13:56:59 +0200

    Add files via upload

--- hover.js
+++ hover.js
@@ -1,13 +1,1 @@
-//Twitter
-var icon = document.querySelector('.icon');
-var nomDuSite = document.querySelector('.nomdusite');
-
-nomDuSite.addEventListener('mouseover', function () {
-	this.classList.add('hovered');
-  icon.classList.add('hovered');
-});
-
-nomDuSite.addEventListener('mouseout', function () {
-	this.classList.remove('hovered');
-  icon.classList.remove('hovered');
-});
+fo copié colé lol
